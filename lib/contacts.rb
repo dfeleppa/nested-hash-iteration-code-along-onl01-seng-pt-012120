a3a074d90674f02e5f9de9f9e1f9d0df5cdfa015
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-02 21:47:11 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,13 +13,14 @@ contacts = {
   }
 }
 
-  
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-end
-> person
-=> "Jon Snow"
- 
-> contact_details_hash
-=> {:name=>"Jon", :email=>"jon_snow@thewall.we", :favorite_ice_cream_flavors=>["chocolate", "vanilla"]}
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
